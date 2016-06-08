8a8cacfb676d7429d52f7e5f03ed55d6c7a92ab0
gartorware <gartorware@gmail.com>
2016-06-08 15:58:27 +0200

    added option select callback property along commit #bf23247

--- src/ionic-modal-select.js
+++ src/ionic-modal-select.js
@@ -38,6 +38,7 @@
 					scope: {
 						initialOptions:"=options",
 						optionGetter:"&",
+                        optionSelectCallback: "&",
 						searchFilters: "=searchFilters",
 						searchProperties:'=',
 						onSelect: "&",
